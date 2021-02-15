ddb13980a93dbece5f884833eed600218d6598f1
William Tomé <william.tomenh@gmail.com>
2021-02-15 00:20:29 -0300

    Test add products in cart.

--- src/Carrinho.php
+++ src/Carrinho.php
@@ -6,5 +6,15 @@ namespace Code;
 
 class Carrinho
 {
+    private $produtos = [];
 
+    public function addProduto($produto)
+    {
+        $this->produtos[] = $produto;
+    }
+
+    public function getProdutos()
+    {
+        return $this->produtos;
+    }
 }
--- tests/CarrinhoTest.php
+++ tests/CarrinhoTest.php
@@ -14,4 +14,26 @@ class CarrinhoTest extends TestCase
         $classe = class_exists("Code\\Carrinho");
         $this->assertTrue($classe);
     }
+
+    public function testAdicaoDeProdutosNoCarrinho()
+    {
+        $produto = new Produto();
+        $produto->setName('Produto 1');
+        $produto->setPrice(19.90);
+        $produto->setSlug('produto-1');
+
+        $produto2 = new Produto();
+        $produto2->setName('Produto 2');
+        $produto2->setPrice(24.90);
+        $produto2->setSlug('produto-2');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto);
+        $carrinho->addProduto($produto2);
+
+        $this->assertIsArray($carrinho->getProdutos());
+        $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[0]);
+        $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[1]);
+
+    }
 }
