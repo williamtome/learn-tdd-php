b8cc5ec3038bdc07165beb466d75e027382d7e34
William Tomé <william.tomenh@gmail.com>
2021-02-15 09:59:21 -0300

    Test if products amount of the cart and total sale of the cart is correct.

--- src/Carrinho.php
+++ src/Carrinho.php
@@ -17,4 +17,19 @@ class Carrinho
     {
         return $this->produtos;
     }
+
+    public function getTotalProdutos()
+    {
+        return count($this->produtos);
+    }
+
+    public function getTotalCompra()
+    {
+        $totalCompra = 0;
+
+        foreach ($this->produtos as $produto)
+            $totalCompra += $produto->getPrice();
+
+        return $totalCompra;
+    }
 }
--- tests/CarrinhoTest.php
+++ tests/CarrinhoTest.php
@@ -8,7 +8,7 @@ class CarrinhoTest extends TestCase
 {
     // Manipular vários produtos.
     // Visualizar produtos.
-    // Total de produtos.
+    // Total de produtos | total compra.
     public function testSeClasseCarrinhoExiste()
     {
         $classe = class_exists("Code\\Carrinho");
@@ -35,5 +35,40 @@ class CarrinhoTest extends TestCase
         $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[0]);
         $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[1]);
 
+    }
+
+    public function testSeValoresDosProdutosNoCarrinhoEstaoCorretosConformePassado()
+    {
+        $produto = new Produto();
+        $produto->setName('Produto 1');
+        $produto->setPrice(19.90);
+        $produto->setSlug('produto-1');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto);
+
+        $this->assertEquals('Produto 1', $carrinho->getProdutos()[0]->getName());
+        $this->assertEquals(19.90, $carrinho->getProdutos()[0]->getPrice());
+        $this->assertEquals('produto-1', $carrinho->getProdutos()[0]->getSlug());
+    }
+
+    public function testSeTotalDeProdutosNoCarrinhoEstaoCorretos()
+    {
+        $produto = new Produto();
+        $produto->setName('Produto 1');
+        $produto->setPrice(19.90);
+        $produto->setSlug('produto-1');
+
+        $produto2 = new Produto();
+        $produto2->setName('Produto 2');
+        $produto2->setPrice(24.90);
+        $produto2->setSlug('produto-2');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto);
+        $carrinho->addProduto($produto2);
+
+        $this->assertEquals(2, $carrinho->getTotalProdutos());
+        $this->assertEquals(44.80, $carrinho->getTotalCompra());
     }
 }
